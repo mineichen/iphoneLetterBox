df337348bed144bafe73c82b38935ed2a125fcc9
iOSKurs <iOSKurs1@n0002146.local>
2014-11-26 14:47:22 +0100

    GUI created. Basic segue configured.

---
+++ smartLetterBox/BenutzerHinzufuegenViewController.h
@@ -0,0 +1,15 @@
+//
+//  BenutzerHinzufuegenViewController.h
+//  smartLetterBox
+//
+//  Created by iOSKurs on 26/11/14.
+//  Copyright (c) 2014 Markus Ineichen & Thomas Jösler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BenutzerHinzufuegenViewController : UIViewController
+- (IBAction)cancelPressed:(UIBarButtonItem *)sender;
+
+
+@end
---
+++ smartLetterBox/BenutzerTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  BenutzerTableViewController.h
+//  smartLetterBox
+//
+//  Created by iOSKurs on 26/11/14.
+//  Copyright (c) 2014 Markus Ineichen & Thomas Jösler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BenutzerTableViewController : UITableViewController
+- (IBAction)unwindToTableView:(UIStoryboardSegue*)sender;
+@end
